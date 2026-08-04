966a9be47128d538db0d943510a8711afa701468
abdul <abdul.tanveer@gmail.com>
2026-08-04 16:54:43 +0530

    life b4 viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 android {
     namespace = "com.example.affirmations"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.affirmations"
---
+++ app/src/main/java/com/example/affirmations/VmViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations
+
+import androidx.lifecycle.ViewModel
+
+//viewmodel = RAM = survivves the config
+class VmViewModel: ViewModel() {
+    var count = 0
+
+}
